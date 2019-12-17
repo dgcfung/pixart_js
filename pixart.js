94c155158ce867639e9462d1385132b3a729dd24
B Legit <fungd@Donalds-MacBook-Air.local>
2019-12-17 18:25:50 -0500

    step 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,23 @@
 
+let setUpEventListeners= function(){
+    check4Submission()
+}
+
+
+
+function check4Submission(){
+    let colorInputField= document.querySelector('#set-color')
+    colorInputField.addEventListener('click', changeColor)
+    console.log(colorInputField)
+    // second argument is always a function, calls changeColor() function
+}
+
+function changeColor(){
+    event.preventDefault()
+    let getInput = document.querySelector("#color-field").value
+    let brush= document.querySelector('.brush')
+    brush.style.background= getInput
+    console.log(getInput)
+}
+
+setUpEventListeners()
